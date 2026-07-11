e2cb8e75620b95603dad1d95b40bf778bcf19db2
Oluwamayowa Oladosu <ceo@layerrail.com>
2026-07-11 04:46:08 -0400

    fix(dashboard): add deployment network route

---
+++ web/apps/dashboard/app/(app)/[workspaceSlug]/projects/[projectId]/apps/[appId]/(overview)/deployments/[deploymentId]/network/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { PageBody } from "@unkey/ui";
+import { DeploymentNetworkSection } from "../(overview)/components/sections/deployment-network-section";
+
+export default function DeploymentNetworkPage() {
+  return (
+    <PageBody>
+      <DeploymentNetworkSection />
+    </PageBody>
+  );
+}
