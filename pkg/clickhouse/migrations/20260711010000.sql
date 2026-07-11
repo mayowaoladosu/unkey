b88b000c4312c8b44bb89fb281af0ccd02f78a44
Oluwamayowa Oladosu <ceo@layerrail.com>
2026-07-11 06:18:25 -0400

    fix(clickhouse): replace resource view atomically

--- pkg/clickhouse/migrations/20260711010000.sql
+++ pkg/clickhouse/migrations/20260711010000.sql
@@ -1,7 +1,6 @@
 -- ClickHouse expands SELECT * when a view is created, so recreate the
 -- convenience view after adding deployment_resource_* columns to its source.
-DROP VIEW IF EXISTS default.instance_checkpoints;
-CREATE VIEW default.instance_checkpoints AS
+CREATE OR REPLACE VIEW default.instance_checkpoints AS
 SELECT *
 FROM default.instance_checkpoints_v1
 FINAL;
